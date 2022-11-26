966319dd6a2714b083f3700ee2cd72a7b63aadc5
JuriiKrutov <juriikrutov@gmail.com>
2022-11-26 13:24:19 +0300

    написал метод для печати массива

--- controlWork_001/Program.cs
+++ controlWork_001/Program.cs
@@ -2,6 +2,15 @@
 первоночальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться
 коллекциями, лучше обойтись исключительно массивами*/
 
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write(array[i]);
+    }
+}
+
 string[] array = {"hello", "2", "world", ":-)" };
 string[] massiv = new string [array.Length];
 int count = 0;
